ffc473bc146d71085c968723ad32b55c62200a6d
Leonardo Souza <leonardo030605@gmail.com>
2021-05-07 08:32:08 -0300

    correção no desafio

--- Desafio/Program.cs
+++ Desafio/Program.cs
@@ -6,17 +6,18 @@ namespace Desafio
     {
         static void Main(string[] args)
         {
-            int valor1 = 0;
+            int valor1 = -1;
             int valor2 = 1;
             int valor3 = 0;
 
             do
             {
                 valor3 = valor1 + valor2;
-                Console.Write(valor3 + ", ");
                 valor1 = valor2;
                 valor2 = valor3;
-            } while (valor2 <= 500);
+                
+                Console.Write(valor3 + ", ");
+            } while (valor3 <= 500);
 
         }
     }
